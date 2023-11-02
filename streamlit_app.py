6b5034a02f0ffe325d8d0a8bf336515130ee4219
navyasri-522 <110251316+navyasri-522@users.noreply.github.com>
2023-11-02 12:37:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas 
+import requests
+import snowflake.connector
+from urllib.error import URLERROR
 streamlit.title('my parents healthy dinner')
  
 streamlit.header('Breakfast Menu')
@@ -9,7 +13,6 @@ streamlit.text(' 🥑🍞 avacado toast')
  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
  
-import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -20,7 +23,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
  
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
  
@@ -29,4 +31,3 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
